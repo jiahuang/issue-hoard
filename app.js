890fadeed6cdedde4954718da10452af72029109
jiahuang <jialiya.huang@students.olin.edu>
2013-01-03 17:31:32 -0500

    setting up webhooks

--- app.js
+++ app.js
@@ -54,60 +54,105 @@ app.use(oauth.middleware(function (req, res, next) {
   user('user').get(function (err, json) {
 
     req.session.userinfo = json;
-    console.log(req.session);
+    console.log(req.session['github.com:oauthAccessToken']);
+    cols.users.update({
+      user: req.session.userinfo.login
+    }, {
+      user: req.session.userinfo.login,
+      token: req.session['github.com:oauthAccessToken'],
+      update_time: new Date()
+    }, {
+      upsert: true
+    }, function (err, docs) {
+      if (err) 
+        console.log("Error inserting user into db", req.session.userinfo);
 
-    res.redirect('/');
+      return res.redirect('/');
+    });
+
   });
 }));
 
 app.get('/', function (req, res) {
 
-  console.log(req.session);
   var user = oauth.session(req);
   if (!user) {
     return res.send('who are you? <a href="/login/">Better login buddy</a>', 404);
   }
 
   // get a list of all the public repos
-  console.log(req.session.userinfo);
-  user('users', req.session.userinfo.login, 'repos').get(function (err, repos) { 
-    console.log(repos);
-    // repos.forEach(function (index, repo) {
-    //   console.log(repo.name);
-    //   repo.name;
-    //   user('repos', 'jiahuang', repo.name).post({
-    //     name: "web",
-    //     active: "true",
-    //     events: ["push"],
-    //     config: {
-    //       url: "http://peaceful-everglades-2301.herokuapp.com/jiahuang/"+repo.name+"/push",
-    //       content_type: "json"
-    //     }
-    //   }, function (err, json) {
-    //     if (err) return console.error('Error creating push webhook:', err);
-    //     console.log(json);
-    //   });
-    // });
+  // user('users', req.session.userinfo.login, 'repos').get(function (err, repos) { 
+  //   // console.log(repos);
+  //   repos.forEach(function (repo, index) {
+  //     console.log(repo.name);
+  //     if (repo.name == 'github-todo') { // just for testing
+  //       user('repos', 'jiahuang', repo.name, 'hooks').post({
+  //         name: "web",
+  //         active: "true",
+  //         events: ["push"],
+  //         config: {
+  //           url: "http://peaceful-everglades-2301.herokuapp.com/users/jiahuang/"+repo.name+"/push",
+  //           content_type: "json"
+  //         }
+  //       }, function (err, json) {
+  //         if (err) return console.error('Error creating push webhook:', err, json);
+  //         console.log(json);
+  //       });
+  //     }
+  //   });
 
     return res.send('Hi there ' + req.session.userinfo.login, 200);
 
-  });
+  // });
 });
 
 app.get('/login', function (req, res) {
   oauth.startSession(req, {
-    scope: ['user', 'repo']
+    scope: ['public_repo']
   }, function (url) {
     console.log(url);
     res.redirect(url);
   });
 });
 
-app.post('/push', function (req, res) {
+app.post('/users/:user/:repo/push', function (req, res) {
+  // get the commit diff
 
+  // check the commit diff for ISSUES
+
+  // for each issue found
+  // if the issue already exists, create another comment
+
+  // if it's new, create it and assign it to someone
+
+  // if it's deleted, close the issue
+  console.log(req.params.user, req.params.repo, req.body);
 });
 
-// Start server.
-var port = parseInt(process.env.PORT || 3000);
-app.listen(port);
-console.log('Server listening on port', port);
+var mongodb, cols = {};
+
+function setupMongo(next) {
+  mongo.connect(process.env.MONGOLAB_URI, {}, function (error, db) {
+    mongodb = db;
+    if (error) {
+      console.error('Error connecting to mongo:', error);
+      process.exit(1);
+    }
+    console.log("Connected to Mongo:", process.env.MONGOLAB_URI);
+
+    mongodb.on("error", function (error) {
+      console.log("Error connecting to MongoLab");
+    });
+
+    cols.users = new mongo.Collection(mongodb, 'users');
+
+    next();
+  });
+}
+
+setupMongo(function () {
+  // Start server.
+  var port = parseInt(process.env.PORT || 3000);
+  app.listen(port);
+  console.log('Server listening on port', port);
+});
