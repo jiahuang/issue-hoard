5c05c3bee3c5b18251635e52b889af190cad75f4
Tim Cameron Ryan <id@timryan.org>
2013-01-03 16:17:54 -0500

    Router before session and old Rem API

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
@@ -33,76 +32,73 @@ app.configure('development', function(){
 });
 
 app.use(express.session({
-  secret: process.env.SECRET,
-  cookie: {
-    domain: process.env.HOST_URL
-  }
+  secret: process.env.SECRET
 }));
 
-var github = rem.load('github', 3, {
+// Has to go after express.session
+app.use(app.router);
+
+var github = rem.connect('github.com', 3.0).configure({
   key: process.env.GH_KEY,
   secret: process.env.GH_SECRET
 });
 
-oauth = rem.oauth(github, "http://"+process.env.HOST_URL+"/oauth/callback/");
+var oauth = rem.oauth(github, "http://"+process.env.HOST_URL+"/oauth/callback/");
 
 // The oauth middleware intercepts the callback url that we set when we
 // created the oauth middleware.
 app.use(oauth.middleware(function (req, res, next) {
   console.log("User is now authenticated.");
-  req.user('user').get(function (err, json) {
+
+  var user = oauth.session(req);
+  user('user').get(function (err, json) {
 
     req.session.userinfo = json;
-    console.log(req.session.oauthAccessToken);
+    console.log(req.session);
 
     res.redirect('/');
   });
 }));
 
-app.get('/', function(req, res) {
-  console.log(req);
+app.get('/', function (req, res) {
 
-  var user = null;
-  github('users', req.session).get(function (err, json) {
-    if (err || !json) {
-      return res.send("well that didn't work", 404);
-    }
-    // console.log("got user", json);
-    user = oauth.session(req);
-    if (!user)
-    return res.send('who are you?', 200);
+  console.log(req.session);
+  var user = oauth.session(req);
+  if (!user) {
+    return res.send('who are you? <a href="/login/">Better login buddy</a>', 404);
+  }
 
-    // get a list of all the public repos
-    user('users', 'jiahuang', 'repos').get(function (err, repos) { 
-      // console.log(repos);
-      // repos.forEach(function (index, repo) {
-      //   console.log(repo.name);
-      //   repo.name;
-      //   user('repos', 'jiahuang', repo.name).post({
-      //     name: "web",
-      //     active: "true",
-      //     events: ["push"],
-      //     config: {
-      //       url: "http://peaceful-everglades-2301.herokuapp.com/jiahuang/"+repo.name+"/push",
-      //       content_type: "json"
-      //     }
-      //   }, function (err, json) {
-      //     if (err) return console.error('Error creating push webhook:', err);
-      //     console.log(json);
-      //   });
-      // });
+  // get a list of all the public repos
+  console.log(req.session.userinfo);
+  user('users', req.session.userinfo.login, 'repos').get(function (err, repos) { 
+    console.log(repos);
+    // repos.forEach(function (index, repo) {
+    //   console.log(repo.name);
+    //   repo.name;
+    //   user('repos', 'jiahuang', repo.name).post({
+    //     name: "web",
+    //     active: "true",
+    //     events: ["push"],
+    //     config: {
+    //       url: "http://peaceful-everglades-2301.herokuapp.com/jiahuang/"+repo.name+"/push",
+    //       content_type: "json"
+    //     }
+    //   }, function (err, json) {
+    //     if (err) return console.error('Error creating push webhook:', err);
+    //     console.log(json);
+    //   });
+    // });
 
-      return res.send('Hi there', 200);
+    return res.send('Hi there ' + req.session.userinfo.login, 200);
 
-    }); 
-
-  });  
+  });
 });
 
 app.get('/login', function (req, res) {
   oauth.startSession(req, {
-    scope: ['']
+    scope: ['user', 'repo']
   }, function (url) {
+    console.log(url);
     res.redirect(url);
   });
 });
