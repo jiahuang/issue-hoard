646ef28dd2dff7d533e779dee1a27bcd98f4d00d
jiahuang <jialiya.huang@students.olin.edu>
2013-01-04 17:04:54 -0500

    working issue creation

--- app.js
+++ app.js
@@ -10,9 +10,10 @@ var ISSUES = require('./issues')
   , oauth = require("oauth")
   , mongo = require('mongodb'), ObjectID = mongo.ObjectID
   , rem = require('rem')
-  , https = require('https');
+  , https = require('https')
+  , DIFF_PARSER = require('./controllers/diff_parser');
 
-
+var DIFF_PARSER = new DIFF_PARSER(ISSUES);
 var app = express();
 
 app.configure(function(){
@@ -115,34 +116,104 @@ app.get('/login', function (req, res) {
   });
 });
 
+// TODO (clean up code): code on /users/:user/:repo/push needs to be cleaned 
 app.post('/users/:user/:repo/push', function (req, res) {
   // find that user's oauth token
   cols.users.findOne({
     user: req.params.user
   }, function (err, userObj) {
+    if (err)
+      return res.send('User '+req.params.user+' is not authenticated with this service.');
+    
+    var commits = req.body.commits; 
+    // console.log(commits);
+    if (!commits || commits.length < 1)
+      return res.send('Post body did not contain a list of commits. Is this webhook set up correctly?');
+    
     var authObj = oauth.restore({oauthAccessToken: userObj.token});
-    // req.session = user;
-    // get the commit diff
-    console.log("req body", req.body);
-    console.log("req commits", req.body.commits);
+    var all_issues = [];
+    // get the list of all issues
+    authObj('repos', req.params.user, req.params.repo, 
+      'issues').get(function (err, curr_issues) {
 
-    authObj('repos', userObj.user, repo, 'compare', 
-      'master...develop').get(function (err, json) { 
-      console.log("diff", json);
+      // var lastCommit = commits.length;
+      commits.forEach(function (commit, index) {
+        // console.log('commit index', index, lastCommit);
+        // if (index == lastCommit -1)
+          // res.send('Made the following issue updates to '+
+            // req.params.user+"'s "+req.params.repo+' repo', all_issues);
+        
+        // get this entire commit
+        // GET /repos/:owner/:repo/git/commits/:sha
+        authObj('repos', req.params.user, req.params.repo, 'git'
+          , 'commits', commit.id).get(function (err, curr_commit) {
+            // get the diff between this commit and its parent
+          authObj('repos', req.params.user, req.params.repo, 'compare', 
+            curr_commit.parents[0].sha+'...'+curr_commit.sha).get(function (err, diff) { 
+            // console.log("diff", json.diff_url);
+            // console.log("diff files", diff.files);
+            diff.files.forEach(function (file){
+              var diff_issues = DIFF_PARSER.parseLines(file.patch);
+              console.log("issues", diff_issues);
+              diff_issues.forEach(function (diff_issue) {
+                var similar_issue = curr_issues.filter(function (curr_issue){
+                  if (curr_issue.similarTo(similar_issue))
+                    return curr_issue;
+                });
+                if (similar_issue.length > 0) {
+                  // we have multiple issues with the same name or this isn't changing anything about the issue
+                  // uhhh no clue which one to edit, let's just skip it
+                  if (similar_issue.length > 1 || similar_issue[0].equals(diff_issue))
+                    return;
+                  
+                  // otherwise it's a patch of the current issue
+                  var curr_labels = similar_issue[0].labels.map(function (label) {
+                    return label.name;
+                  });
+                  if (curr_labels.indexOf(diff_issue.label) == -1) {
+                    curr_labels.push(diff_issue.label);
+                  }
+                  var patch_issue = {
+                      title: diff_issue.title, 
+                      body: diff_issue.comment,
+                      assignee: diff_issue.getAssignee(curr_commit.author.name),
+                      status: diff_issue.status,
+                      labels: curr_labels
+                    };
+
+                  authObj('repos', req.params.user, req.params.repo, 'issues'
+                    , similar_issue[0].number).patch( patch_issue
+                    , function (err, json) {
+                      if (err)
+                        return console.log("Failed to patch issue ", patch_issue, err);
+                      
+                      // if (!err)
+                        // all_issues.push(patch_issue)
+                  });
+                } else if (diff_issue.isOpen()) {
+                  // we're creating a new issue
+                  var create_issue = {
+                    title: diff_issue.title,
+                    body: diff_issue.comment,
+                    assignee: diff_issue.getAssignee(curr_commit.author.name),
+                    labels: [diff_issue.label]
+                  };
+
+                  authObj('repos', req.params.user, req.params.repo, 'issues').post(
+                    create_issue , function (err, json) {
+                      if (err)
+                        return console.log("Failed to create issue ", create_issue, err, json);
+                      // if (!err)
+                        // all_issues.push(create_issue);
+                  });
+                }
+              });
+            });
+          });
+        });
+      });
     });
-
   });
-
-  // check the commit diff for ISSUES
-
-  // for each issue found
-  // if the issue already exists, create another comment
-
-  // if it's new, create it and assign it to someone
-
-  // if it's deleted, close the issue
-  console.log(req.params.user, req.params.repo, req.body);
-  return res.send("hit push endpoint");
 });
 
 var mongodb, cols = {};
